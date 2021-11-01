66a61fdae55642ab5e94078bde45aad3026abb4a
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2021-11-01 13:45:24 -0400

    add Over lay component

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -2,7 +2,7 @@ import React from "react";
 import Card from "./Card";
 import Button from "./Button";
 import classes from "./ErrorModal.module.css";
-import ReactDOM from 'react-dom'
+import ReactDOM from "react-dom";
 
 const Backdrop = (props) => {
   return <div className={classes.backdrop} onClick={props.onConfirm} />;
@@ -25,9 +25,22 @@ const ModalOverlay = (props) => {
 };
 
 const ErrorModal = (props) => {
-  return <React.Fragment>
-    {ReactDOM.createPortal(<Backdrop onConfirm={props.onConfirm}/>,document.getElementById("backdrop-root"))}
-  </React.Fragment>;
+  return (
+    <React.Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById("overlay-root")
+      )}
+    </React.Fragment>
+  );
 };
 
 export default ErrorModal;
